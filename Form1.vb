cc66d9432e292ef548ad9e0b6e713211e1ef063b
Dell-User <Dell-User@DESKTOP-78SJPVI>
2023-10-04 15:12:21 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'Declaration of variables
+
+        Dim height As Double
+        Dim weight As Double
+        Dim bmi As Double
+
+        weight = Val(TextBox1.Text)
+        height = Val(TextBox2.Text)
+
+        bmi = weight / (height) ^ 2 'BMI formula
+
+        TextBox3.Text = bmi
+
+        If bmi < 18 Then
+            TextBox4.Text = "you are underweight"
+        ElseIf 18 <= bmi And bmi < 26 Then
+            TextBox4.Text = "you are normal"
+
+        Else
+            TextBox4.Text = "you are overweight"
+        End If
+    End Sub
+End Class
